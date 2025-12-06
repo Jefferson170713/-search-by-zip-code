ccfd08ba20cfb0576427089ac0cdae4b30030cc2
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-12-06 15:04:07 +0000

    refactor(script): tratando erro da página

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,41 @@
 // const intputCep = document.getElementById('cep');
 // console.log(intputCep);
 
-const input = document.querySelector('#cep')
-const button = document.getElementById("btn-buscar")
-        
+const input = document.querySelector('#cep');
+const button = document.getElementById('btn-buscar');
+
+const getLogradouro = document.getElementById('logradouro');
+const getBairro = document.getElementById('bairro');
+const getCidade = document.getElementById('cidade');
+const getEstado = document.getElementById('estado');
+const resultado = document.getElementById('resultado')
+
+const erroHidden = resultado.classList.contains('hidden')
+
 button.addEventListener('click', async (e)=>{
     const inputValue = input.value;
     const data = await fecthData(inputValue);
-    console.log(data);
+
+    getLogradouro.textContent = data.logradouro;
+    getBairro.textContent = data.bairro;
+    getCidade.textContent = data.localidade;
+    getEstado.textContent = data.estado;
+
+    if(data.erro == 'true') {
+        if(erroHidden) {
+            resultado.classList.add('hidden');
+            console.log(erroHidden);
+        }
+    }
+    else {
+        console.log(erroHidden);
+        resultado.classList.remove('hidden');
+    }
+    // console.log(data);
+    // console.log(getLogradouro);
+    // console.log(getBairro);
+    // console.log(getCidade);
+    // console.log(getEstado);
 });
 
 async function fecthData(cep) {
